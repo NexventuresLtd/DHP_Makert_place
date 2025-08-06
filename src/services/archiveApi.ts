0ddace377feb4036162c8d7840af061f9e672ebf
amuhirwa <a.muhirwa@alustudent.com>
2025-08-06 16:22:37 +0200

    fix: wrong api url

--- src/services/archiveApi.ts
+++ src/services/archiveApi.ts
@@ -1,6 +1,6 @@
 import type { PaginatedResponse } from "../types";
 
-const API_BASE_URL = 'http://127.0.0.1:8000/api/digital-repo';
+const API_BASE_URL = `${import.meta.env.VITE_API_BASE_URL}/api/digital-repo`;
 
 export interface ArchiveType {
   id: number;
--- src/services/digitalContentApi.ts
+++ src/services/digitalContentApi.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://127.0.0.1:8000/api/digital-repo';
+const API_BASE_URL =  `${import.meta.env.VITE_API_BASE_URL}/api/digital-repo`;
 
 export interface DigitalContentType {
   id: number;
