4afabbd9954f97fe2aa0592020e06de8956c9d85
lscblack <l.christian@alustudent.com>
2025-08-07 12:03:36 +0200

    Fixed Plus icons on tje login

--- src/comps/MarketPlace/heroSection/Header.tsx
+++ src/comps/MarketPlace/heroSection/Header.tsx
@@ -170,9 +170,9 @@ const Header = ({
                                             aria-label="Shopping cart"
                                         >
                                             <ShoppingCart className="w-5 h-5 lg:w-6 lg:h-6" />
-                                            <span className="absolute -top-1 -right-1 w-5 h-5 bg-primary text-white text-xs rounded-full flex items-center justify-center font-medium border border-emerald-100">
+                                            {/* <span className="absolute -top-1 -right-1 w-5 h-5 bg-primary text-white text-xs rounded-full flex items-center justify-center font-medium border border-emerald-100">
                                                 +
-                                            </span>
+                                            </span> */}
                                         </button>
                                     </div>
 
